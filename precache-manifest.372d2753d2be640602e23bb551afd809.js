42247b290d5a4fdd8519bc4fb205a5923dae663b
JJs-fun-stuff <worameth.jojo@gmail.com>
2020-06-01 11:42:52 +0700

    Updates

---
+++ precache-manifest.372d2753d2be640602e23bb551afd809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c9914ef94938f215d4fc20e0bfb5c4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "345c0be57c2521d31a7d",
+    "url": "/monsters-rolodex/static/css/main.d834662d.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345c0be57c2521d31a7d",
+    "url": "/monsters-rolodex/static/js/main.b02359e7.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
